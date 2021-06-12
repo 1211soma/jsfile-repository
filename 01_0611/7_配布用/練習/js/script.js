bf1d0b3df6a4b7386efd7a8b1b82ec90bc434391
tsuji soma <21aw0129@jec.ac.jp>
2021-06-13 01:12:04 +0900

    switch文の復習　論理和のところ　復習

--- 01_0611/7_配布用/練習/js/script.js
+++ 01_0611/7_配布用/練習/js/script.js
@@ -1,29 +1,7 @@
-
-var num = 5;
-
-for (let i = 0; i < 5; i++){ /**（初期化式;  条件式;  加算式;） */
-    console.log(i * num);
-}
-
-/**インクリメント　検証 */
-let x = 1;
-let y;
-
-y = ++x;
-console.log(x);
-console.log(y);
-
-var array = [];
-array = [2,4,6,8,10];
-array.length
-
-for ( let i = 0; i < 5 ; i++){
-    console.log(array[i]);  /**[i]という指定はinedexのすべてを表示させてください */ 
-    /**pushで最後尾に値を追加するときは、i < n を1増やす*/
-    
-
-
-}
+alert( 1 && 2 ); 
+//alert( null || 2 ); // 2 (2 は最初のtruthy) //２が最初に帰ってくる
+//alert( null || 0 || 6 ); // 2 (最初のtruthy)
+//alert( undefined || null || 0 ); // 0 (すべて偽、なので最後の値が返却される)
 
 
 
