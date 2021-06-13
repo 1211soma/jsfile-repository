67e3ace14534c949931816162256de63f7d9c5c1
tsuji soma <21aw0129@jec.ac.jp>
2021-06-13 14:16:32 +0900

    and or 復習

--- 01_0611/7_配布用/練習/js/script.js
+++ 01_0611/7_配布用/練習/js/script.js
@@ -1,7 +1,16 @@
-alert( 1 && 2 ); 
+alert ( null && 2 && 3 && 4 );
 //alert( null || 2 ); // 2 (2 は最初のtruthy) //２が最初に帰ってくる
 //alert( null || 0 || 6 ); // 2 (最初のtruthy)
 //alert( undefined || null || 0 ); // 0 (すべて偽、なので最後の値が返却される)
+
+var x = 3;
+var y = 4;
+var ans = x + y;
+
+
+var x = 14;
+var ans2= x + y; // 18 と返される 
+//var は　後から数値を変更できる　変数　であることが　証明　された。
 
 
 
