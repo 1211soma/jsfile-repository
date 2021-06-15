4745744e062a69aee9e726911b4f658771b15bab
tsuji soma <21aw0129@jec.ac.jp>
2021-06-16 00:30:01 +0900

    復習　３回目

--- 01_0611/7_配布用/練習/js/script.js
+++ 01_0611/7_配布用/練習/js/script.js
@@ -1,4 +1,4 @@
-alert ( null && 2 && 3 && 4 );
+alert ( null || 78 || null || undefined);
 //alert( null || 2 ); // 2 (2 は最初のtruthy) //２が最初に帰ってくる
 //alert( null || 0 || 6 ); // 2 (最初のtruthy)
 //alert( undefined || null || 0 ); // 0 (すべて偽、なので最後の値が返却される)
