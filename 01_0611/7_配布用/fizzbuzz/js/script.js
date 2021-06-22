d61d8ac854fa29baef976e36c1289218a10163ac
tsuji soma <21aw0129@jec.ac.jp>
2021-06-22 20:25:40 +0900

    テスト対策

--- 01_0611/7_配布用/fizzbuzz/js/script.js
+++ 01_0611/7_配布用/fizzbuzz/js/script.js
@@ -2,15 +2,16 @@ var fizzbuzz = [];
 
 for (let i = 1; i < 101; i++){
   if ( i % 15 == 0){
-    console.log("fizzbuz");
+    console.log ('fizzbuz');
   }else if( i % 5 == 0){
-    console.log("buzz");
-  }else if(i % 3 == 0){
-    console.log("fizz");
+    console.log ('buzz');
+  }else if( i % 3 == 0){
+    console.log ('fizz')
   }else {
-    console.log(i);
+    console.log (i);
   }
 }
+ 
  
 //ここまで
 
