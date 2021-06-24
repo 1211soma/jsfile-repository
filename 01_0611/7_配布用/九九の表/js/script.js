66fcfa6b322ddf0d9fce5f7d5a39e8853eb25818
tsuji soma <21aw0129@jec.ac.jp>
2021-06-24 09:10:10 +0900

    九九　復習

--- 01_0611/7_配布用/九九の表/js/script.js
+++ 01_0611/7_配布用/九九の表/js/script.js
@@ -1,15 +1,15 @@
 // 下記の6つの処理を各1回ずつとfor文を使って九九の表を作成しよう
 var kuku = document.querySelector('.kuku'); //table要素を取得
-var tr = document.createElement('tr'); //trタグを生成
-var td = document.createElement('td'); //tdタグを生成  
-kuku.appendChild(tr); //tableタグ内にtrを挿入
-tr.appendChild(td); //trタグ内にtdを挿入
-//td.innerText = (i * j); //td内に計算結果を挿入
 
 for ( let i = 1; i < 10; i++){
+    var tr = document.createElement('tr'); //trタグを生成
+    kuku.appendChild(tr); //tableタグ内にtrを挿入
     for ( let j = 1; j < 10; j++){
-    console.log ( i * j );   
+    var td = document.createElement('td'); //tdタグを生成  
+    tr.appendChild(td); //trタグ内にtdを挿入
+    td.innerText = (i * j); //td内に計算結果を挿入
 
+    console.log ( i * j );   
  }
 }
 
