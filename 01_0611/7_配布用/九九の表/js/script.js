23d016f549408df28149aff3ba3ac207c3b7de8c
tsuji soma <21aw0129@jec.ac.jp>
2021-06-26 12:26:27 +0900

    js 復習

--- 01_0611/7_配布用/九九の表/js/script.js
+++ 01_0611/7_配布用/九九の表/js/script.js
@@ -4,10 +4,12 @@ var kuku = document.querySelector('.kuku'); //table要素を取得
 for ( let i = 1; i < 10; i++){
     var tr = document.createElement('tr');
     kuku.appendChild(tr);
+    console.log(tr);
     for ( let j = 1; j < 10; j++){
     var td = document.createElement('td');
     tr.appendChild(td);
     td.innerText = ( i * j );
+    console.log(td);
     }
 }
 
