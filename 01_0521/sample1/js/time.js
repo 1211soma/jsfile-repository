e389448c9ee180c3b6ff0072b411f5a369c2d427
tsuji soma <21aw0129@jec.ac.jp>
2021-07-24 13:02:10 +0900

    js復習

--- 01_0521/sample1/js/time.js
+++ 01_0521/sample1/js/time.js
@@ -1,18 +1,20 @@
-var hour = new Date(); /*何時？かは、常に新しい、現在時刻で設定する*/
-var time = hour.getHours();　/*何時？の値を０～２３時までの値で返す*/
+var hour = new Date();
+var time = hour.getHours();
 
-var hello = time; //現在時刻に合わせた文章を指定するための変数名
+var hello = time;
 
-if ( time < 9 ){
-    hello = 'おはよう'; 
-}else if ( time < 12){
+if ( time <= 9){
+    hello = 'おはよう';
+}else if ( time <= 12){
     hello = 'こんにちは';
-}else if ( time < 18){
+}else if ( time <= 18){
     hello = 'こんばんは';
-}else {
-    hello = '夜中にどうしたの';
+}else{
+    hello = 'おやすみ'
 }
 
+var now = document.querySelector('.now');
+now.innerText = hello;
 
 
 
@@ -20,5 +22,4 @@ if ( time < 9 ){
 
 
 
-var now = document.querySelector('.now');　/*varで変数名[now]を設定、htmlのclass名、nowを呼ぶ*/
-now.innerText = hello; /*nowの中にhelloの文言を指定する*/
+
