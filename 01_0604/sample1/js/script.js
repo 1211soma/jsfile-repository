259d903cedcb710292689eed29bad0980f8093fc
tsuji soma <21aw0129@jec.ac.jp>
2021-08-03 14:16:04 +0900

    修正

--- 01_0604/sample1/js/script.js
+++ 01_0604/sample1/js/script.js
@@ -1,28 +1,25 @@
-function bloodcheck(){
+function bloodcheck (){
   var age = document.kenketsu.age.value;
   var blood = document.kenketsu.bloodType.value;
-  var judgeP = document.querySelector('.judge');
+  var judgeP = document.querySelector('.judge')
 
-  if (age == "" || blood == ""){
+  if ( age == "" || blood == ""){
     judgeP.innerText = '必要項目を入力してください';
-  }else if ( age >= 18 && age <= 65 && blood != 'unknown'){
-    judgeP.innerText = 'ご協力ありがとうございました。';
-  } else {
+  }else if ( 18 <= age && 65 >= age && blood != 'unknown'){
+    judgeP.innerText = '不足しています！ご協力お願いします';
+  }else {
     judgeP.innerText = '関心を持っていただきありがとうございました。'
   }
+
   var ageP = document.querySelector('.age');
   var bloodP = document.querySelector('.blood');
   ageP.innerText = age;
   bloodP.innerText = blood;
 
-
 }
 
 var btn = document.querySelector('#btn');
 btn.addEventListener('click',bloodcheck);
-
-
-
 
 //年齢と血液型が空だったらという条件
 //18歳以上65歳以下で、血液型が「不明」になっていない　という条件
