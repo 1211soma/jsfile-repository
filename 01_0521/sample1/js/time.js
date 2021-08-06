a17424aaba22bab0e60fe90ec47a8acd92262d4e
tsuji soma <21aw0129@jec.ac.jp>
2021-08-06 16:51:44 +0900

    相変わらずjsの復習してます

--- 01_0521/sample1/js/time.js
+++ 01_0521/sample1/js/time.js
@@ -3,19 +3,27 @@ var time = hour.getHours();
 
 var hello = time;
 
-if (time < 12){
-    hello = 'おはよう';
-}else if (time < 17){
-    hello = 'こんにちは';
-}else if (time < 21){
-    hello = 'こんばんは';
+if ( time < 12){
+    hello = 'おはよう諸君！';
+}else if ( time < 15){
+    hello = 'もう、お昼を過ぎたね！';
+}else if ( time < 21){
+    hello = 'さて、そろそろ夜中の時間帯だ！';
 }else {
-    hello = '夜中にどうしたの？';
+    hello = '夜中にどうしたんだね？君？';
 }
-
-
-
 
 var now = document.querySelector('.now');
 now.innerText = hello;
+
+
+
+
+
+
+
+
+
+
+
 
--- 01_0604/sample1/js/script.js
+++ 01_0604/sample1/js/script.js
@@ -1,28 +1,35 @@
-function bloodcheck (){
-  var age = document.kenketsu.age.value;
+function bloodcheck(){
   var blood = document.kenketsu.bloodType.value;
-  var judgeP = document.querySelector('.judge')
+  var age = document.kenketsu.age.value;
+  var judgeP = document.querySelector('.judge');
 
   if ( age == "" || blood == ""){
-    judgeP.innerText = '必要項目を入力してください';
+      judgeP.innerText = '必要項目を入力してください';
   }else if ( 18 <= age && 65 >= age && blood != 'unknown'){
-    judgeP.innerText = '不足しています！ご協力お願いします';
+      judgeP.innerText = '不足しています！';
   }else {
-    judgeP.innerText = '関心を持っていただきありがとうございました。'
+    judgeP.innerText = '興味・関心を持っていただきありがとうございました。';
   }
+
 
   var ageP = document.querySelector('.age');
   var bloodP = document.querySelector('.blood');
   ageP.innerText = age;
   bloodP.innerText = blood;
 
+
 }
 
-var btn = document.querySelector('#btn');
+
+var btn = document.getElementById('btn');
 btn.addEventListener('click',bloodcheck);
 
+
+
+
+
 //年齢と血液型が空だったらという条件
-//18歳以上65歳以下で、血液型が「不明」になっていない　という条件
+//18歳以上65歳以下で、血液型が「不明」になっていないという条件
 //それ以外
 
 //年齢と血液型をHTMLに表示させる処理
