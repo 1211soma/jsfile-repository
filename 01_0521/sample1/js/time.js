c58b341afee6cda4b84305ba3932039902ff6825
tsuji soma <21aw0129@jec.ac.jp>
2021-08-14 10:40:40 +0900

    js 復習

--- 01_0521/sample1/js/time.js
+++ 01_0521/sample1/js/time.js
@@ -1,23 +1,3 @@
-var hour = new Date();
-var time = hour.getHours();
-
-var hello = time;
-
-if ( time < 12){
-    hello = 'おはよう諸君！';
-}else if ( time < 15){
-    hello = 'もう、お昼を過ぎたね！';
-}else if ( time < 21){
-    hello = 'さて、そろそろ夜中の時間帯だ！';
-}else {
-    hello = '夜中にどうしたんだね？君？';
-}
-
-var now = document.querySelector('.now');
-now.innerText = hello;
-
-
-
 
 
 
