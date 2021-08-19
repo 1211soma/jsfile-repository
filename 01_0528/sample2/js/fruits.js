a1c1b1222fa605d0d15d0fd2b7f98a8d84f0e71b
tsuji soma <21aw0129@jec.ac.jp>
2021-08-19 21:14:31 +0900

    swt文　css追加

--- 01_0528/sample2/js/fruits.js
+++ 01_0528/sample2/js/fruits.js
@@ -1,47 +1,30 @@
-// ボタンを押したら動くプログラムの塊（関数といいます）
-// 関数はいつでも呼び出せるものなので、順序は関係ありません。47行目に呼応して動作します。
-function itemPrice(){ 
+function itemPrice(){
+    var box = document.fruits.fruit;
+    var num = box.selectedIndex;
+    var optionvalue = box.options[num].value;
+    var cart = document.querySelector('.cart');
 
-    // 変数itemにHTMLのselect要素を取得
-    const item = document.fruits.fruit;　
-
-    // 変数itemに入っているoptionの中で選択した番号を変数numに取得
-    // 0からカウントするのでリンゴなら2がnumに入る、ももなら5。
-    let num = item.selectedIndex;
-
-    // select要素内のoptionの中でnumに指定された番号を持つoptionのvalueを取得。
-    let optionValue = item.options[num].value;
-
-    // 文字を表示するためにp.cartを取得してelmとする。
-    // 後ほどテキストを表示する時にelm.innerTextなどをつかう。
-    let elm = document.querySelector('.cart');
-
-
-    //ここからswitchを記述
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //ここまでswitch
-
-//関数ブロックを閉じる括弧
+    switch (optionvalue){
+        case "":
+            cart.innerText = '商品を選択してください';
+        break;
+        case "バナナ":
+            cart.innerText = 'バナナのたたき売り';
+        break;
+        case "リンゴ":
+            cart.innerText = 'リンゴ飴';
+        break;
+        case "みかん":
+            cart.innerText = 'ミカンジュース';
+        break;
+        case "ぶどう":
+            cart.innerText = 'ぶどうワイン';
+        break;
+        default:
+            cart.innerText = '現在。品切れです';
+        break;
+    }
 }
 
-// ボタン要素を取得して変数btnに代入
-var btn;
-
-// btnに格納された要素をクリックしたら関数itemPriceを動かす
-btn.addEventListener('click',itemPrice)
+var btn = document.getElementById('btn');
+btn.addEventListener('click',itemPrice);
