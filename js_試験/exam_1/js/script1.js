ad4845da7a4c88430d4256feccc4412fc4799d95
tsuji soma <21aw0129@jec.ac.jp>
2021-08-23 11:35:24 +0900

    js試験　復習

--- js_試験/exam_1/js/script1.js
+++ js_試験/exam_1/js/script1.js
@@ -1,7 +1,11 @@
-var elm = document.querySelector('#elm');
-var obj = document.createElement('h1');
+var h1Box = document.createElement('h1');
+var elm = document.getElementById('elm');
 
-var add_obj = elm.appendChild(obj);
-var H1 = document.querySelector('h1');
-H1.innerText = 'hello world';
-H1.className = "red";
+
+elm.appendChild(h1Box);
+
+h1Box.innerText = 'hello world';
+h1Box.className = 'red';
+
+
+
--- js_試験/exam_2/js/script2.js
+++ js_試験/exam_2/js/script2.js
@@ -1,26 +1,21 @@
-//prompt()はファイルを開いたときに表示される入力用ダイアログ
-//※入力用ダイアログは半角数字で入力しないとダメ！！！！
-var Eng = parseInt(prompt('英語の得点を入力してください'));
-var Math = parseInt(prompt('数学の得点を入力してください'));
-var para = document.querySelector('p');
-var jdg; //判定結果用の変数
+var eng = parseInt (prompt ('英語の点数を入力してください ※半角数字で'));
+var math = parseInt(prompt ('数学の点数を入力してください　※半角数字で'));
+var para = document.getElementById('para');
+var jdg;
 
-//数学、英語ともに70点以上で合格とする。
-//どちらも合格の場合は「どちらも合格！おめでとう！」と表示
-//片方の教科だけが合格の場合は「'教科名'が不合格頑張れ！」と表示
-//どちらも不合格の場合は「どちらも不合格！努力しよう！」と表示
 
-//プログラムを記述
-if ( 70 <= Eng && 70 <= Math){
-    jdg = 'どちらも合格！おめでとう！';
-}else if ( 70 <= Eng && 70 > Math){
-    jdg = '数学が不合格頑張れ！';
-}else if ( 70 > Eng && 70 <= Math){
-    jdg = '英語が不合格頑張れ！';
+if ( 70 <= eng && 70 <= math){
+    jdg = 'おめでとう';
+}else if ( 70 >= eng && 70 <= math){
+    jdg = '英語が不合格 残念';
+}else if ( 70 <= eng && 70 >= math){
+    jdg = '数学が不合格 残念';
 }else {
-    jdg = 'どちらも不合格！努力しよう！';
+    jdg = '残念';
 }
 
 
-//プログラム終了
-para.innerText = jdg;   
+para.innerText = jdg;
+
+
+
