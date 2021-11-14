902b718943becca4b707c389e8501f6c3b240c40
tsuji soma <21aw0129@jec.ac.jp>
2021-11-15 08:24:54 +0900

    js ファイル整理

--- 01_1108/kadai3/js/script.js
+++ 01_1108/kadai3/js/script.js
@@ -1,40 +1,62 @@
 'use strict'
 
-
 const todo = document.getElementById('todo');
 const doing = document.getElementById('doing');
 const inputValue = document.querySelector('section > input');
 
-// doingにタスクを移動する
-const toDoingTask = () => {
-  [...todo.querySelectorAll('.to-doing')].forEach((task) =>
-  task.addEventListener('click',() => {
+//doingにタスクを移動
+const toDoingTask =()=>{
+  [...todo.querySelectorAll('.to-doing')].forEach((task)=>
+  task.addEventListener('click',()=>{
     const todoTask = task.parentNode;
-    console.log(todoTask); 
-    const doingList = [...doing.querySelectorAll('.doing-task')].map
-    ((task)=> task.outerHTML);
+    const doingList = [...doing.querySelectorAll('.doing-task')].map((task)=>
+    task.outerHTML);
+    console.log(doingList)
 
-
-    const addDoingTask = () => {
-      return `
-      <li class="todo-task">
+    const addDoingTask =()=>{
+        return `<li class="doing-task">
       <span>${todoTask.querySelector('span').innerText}</span>
       <button class="edit">編集</button>
-      <button class="to-doing">作業開始</button>
+      <button class='undo'>戻す</button>
       <button class="delete">削除</button>
-      </li>
-      `
+      </li>`
     }
-    doing.innerHTML = [...doingList,addDoingTask()].reduce((prev,current)=> prev + current);
+
+    doing.innerHTML = [...doingList,addDoingTask()].reduce((prev,current)=>
+    prev + current);
     task.parentNode.remove();
     deleteTask();
     editTask();
+    undo();
   }))
 }
 
+//todoに戻す
+const undo =()=>{
+  [...doing.querySelectorAll('.undo')].forEach((task)=>
+  task.addEventListener('click',()=>{
+    const doingTask = task.parentNode;
+    const todoList = [...todo.querySelectorAll('.todo-task')].map((task)=>
+    task.outerHTML);
+    console.log(todoList)
 
+    const addUndo =()=>{
+        return  `<li class='todo-task'>
+      <span>${doingTask.querySelector('span').innerText}</span>
+      <button class="edit">編集</button>
+      <button class='to-doing'>作業開始</button>
+      <button class="delete">削除</button>
+      </li>`
+    }
 
-
+    todo.innerHTML = [...todoList,addUndo()].reduce((prev,current)=>
+    prev + current);
+    task.parentNode.remove();
+    deleteTask();
+    editTask();
+    toDoingTask();
+  }))
+}
 
 // 編集
 const editTask = () => [...document.querySelectorAll('.edit')].forEach
@@ -67,19 +89,14 @@ const addTask = ()=> {
   prev + current);
   deleteTask();
   editTask();
-  toDoingTask();
-
-
+  toDoingTask()
+  inputValue.value = ''
 }
 
 
 // 何も入力しなかった場合、アラート起動
 const inputForm = ()=> document.getElementById('btn').
-addEventListener('click',()=> inputValue.value === '' ? alert('タスクを入力しろ、バカヤロー')
+addEventListener('click',()=> inputValue.value === '' ? alert('タスクを入力してください')
 : addTask());
 inputForm();
-
-
-
-
 
